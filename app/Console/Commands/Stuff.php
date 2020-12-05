9c808aa9cd893ef6059faed382283aae4b1d4814
Selahattin GÜNEŞ <36725371+selahattingunes@users.noreply.github.com>
2020-12-05 16:47:17 +0300

    Stuff command oluşturuldu.

---
+++ app/Console/Commands/Stuff.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class Stuff extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'stuff';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Optimize clear';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Artisan::call("optimize:clear");
+        echo Artisan::output();
+        Artisan::call("view:clear");
+        echo Artisan::output();
+    }
+}
